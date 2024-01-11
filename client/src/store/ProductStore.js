b495d28006a4b48786da9a7a2e10e4b648209f84
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-01-11 22:05:49 +0600

    :

---
+++ client/src/store/ProductStore.js
@@ -0,0 +1,36 @@
+import create from "zustand";
+import axios from "axios";
+
+const ProductStore = create((set) => ({
+  BrandList: null,
+  CategoryList: null,
+  SliderList: null,
+  ListProduct: null,
+
+  BrandListRequest: async () => {
+    let res = await axios.get(`/api/v1/ProductBrandList`);
+    if (res.data["status"] === "success") {
+      set({ BrandList: res.data["data"] });
+    }
+  },
+  CategoryListRequest: async () => {
+    let res = await axios.get("/api/v1/ProductCategoryList");
+    if (res.data["status"] === "success") {
+      set({ CategoryList: res.data["data"] });
+    }
+  },
+  SliderListRequest: async () => {
+    let res = await axios.get("/api/v1/ProductSliderList");
+    if (res.data["status"] === "success") {
+      set({ SliderList: res.data["data"] });
+    }
+  },
+  ListByRemarkRequest: async (Remark) => {
+    let res = await axios.get(`/api/v1/ProductListByRemark/${Remark}`);
+    if (res.data["status"] === "success") {
+      set({ ListByRemark: res.data["data"] });
+    }
+  },
+}));
+
+export default ProductStore;
