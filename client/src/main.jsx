d1d95ae85852de155d919adcea08c7488c9d12fa
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-01-11 21:34:48 +0600

    skeleton-add

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import "bootstrap";
 import "./assets/css/main.css";
 import "./assets/css/animate.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
+import "react-loading-skeleton/dist/skeleton.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import SliderSkeleton from "../skeleton/SliderSkeleton";
+import FeaturesSkeleton from "../skeleton/FeaturesSkeleton";
+import CategoriesSkeleton from "../skeleton/CategoriesSkeleton";
+import ProductsSkeleton from "../skeleton/ProductsSkeleton";
+import BrandSkeleton from "../skeleton/BrandSkeleton";
 
 const HomePage = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <SliderSkeleton />
+      <FeaturesSkeleton />
+      <CategoriesSkeleton />
+      <ProductsSkeleton />
+      <BrandSkeleton />
+    </Layout>
+  );
 };
 
 export default HomePage;
