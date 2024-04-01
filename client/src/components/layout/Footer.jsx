f409c386d0ee4632bc211e9b843acb620fd00cb7
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-04-01 21:46:59 +0600

    link-changes

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import pay from "../../assets/images/pay.png";
 const Footer = () => {
   return (
     <div>
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="container py-5">
           <div className="row">
             <div className="col-md-4">
-              <h2 className="bodyMedium">Legals</h2>
+              <h1 className="bodyMedium">Legals</h1>
               <p className="my-2">
                 <Link className="nav-link" to="/about">
                   About
@@ -52,20 +52,15 @@ const Footer = () => {
               <h1 className="bodyMedium">About</h1>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum
+                industry. Lorem Ipsum{" "}
               </p>
-              <img
-                className="w-75"
-                src="https://thumbs.dreamstime.com/z/delhi-india-february-popular-credit-card-companies-logos-including-mastercard-visa-american-express-more-211749084.jpg?ct=jpeg"
-              />
+              <img className="w-100" src={pay} />
             </div>
           </div>
         </div>
       </div>
       <div className="bg-dark py-3 text-center">
-        <p className="text-white bodySmal">
-          &copy; Jahid Hasan - All Rights Reserved
-        </p>
+        <p className="text-white bodySmal">All Rights Reserved </p>
       </div>
     </div>
   );
